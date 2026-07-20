8ca530211321e85f3f43ceed8a8e487678132b07
Thanduongw <levuthanhduong2004@gmail.com>
2026-07-20 17:16:57 +0700

    chore: init monorepo structure - 7 spring boot services + react frontend + docker infra

---
+++ docker/postgres-init/init-databases.sql
@@ -0,0 +1,6 @@
+CREATE DATABASE ats_auth;
+CREATE DATABASE ats_masterdata;
+CREATE DATABASE ats_recruitment;
+CREATE DATABASE ats_candidate;
+CREATE DATABASE ats_interview;
+CREATE DATABASE ats_notification;
---
+++ masterdata-service/src/main/java/iuh/fit/se/MasterdataServiceApplication.java
@@ -0,0 +1,13 @@
+package iuh.fit.se;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterdataServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MasterdataServiceApplication.class, args);
+	}
+
+}

629fee8d20fb3834d516fa6117015140d189d164
Thanduongw <levuthanhduong2004@gmail.com>
2026-07-23 13:58:01 +0700

    feat(masterdata-service): Enhance security configuration and update routing for guest access

--- frontend/src/routes/AppRoutes.tsx
+++ frontend/src/routes/AppRoutes.tsx
@@ -1,4 +1,5 @@
 import { Routes, Route, Navigate } from "react-router-dom";
+import GuestRoute from "../components/GuestRoute";
 import RegisterPage from "../features/auth/pages/RegisterPage";
 import VerifyEmailPage from "../features/auth/pages/VerifyEmailPage";
 import LoginPage from "../features/auth/pages/LoginPage";
@@ -9,9 +10,11 @@ import ProtectedRoute from "../components/ProtectedRoute";
 export default function AppRoutes() {
     return (
         <Routes>
-            <Route path="/register" element={<RegisterPage />} />
-            <Route path="/verify-email" element={<VerifyEmailPage />} />
-            <Route path="/login" element={<LoginPage />} />
+            <Route element={<GuestRoute />}>
+                <Route path="/register" element={<RegisterPage />} />
+                <Route path="/verify-email" element={<VerifyEmailPage />} />
+                <Route path="/login" element={<LoginPage />} />
+            </Route>
 
             <Route element={<ProtectedRoute />}>
                 <Route path="/dashboard" element={<DashboardPage />} />
